434cb251d377a71ac3a07d7aa7cf4784ecbde9ef
sindhu06prakash-oss <sindhu06prakash@gmail.com>
2026-07-08 11:58:37 +0530

    Fixed Render port

--- Flask/app1.py
+++ Flask/app1.py
@@ -105,5 +105,8 @@ def submit():
 # ===========================
 # Run App
 # ===========================
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
